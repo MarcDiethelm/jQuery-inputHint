21e6f0fda8b641f70e689e5ceaafe01dd10c7ad6
Marc Diethelm <boo@chic-happens.ch>
2010-11-12 22:51:47 +0100

    adding initial work to repo.

---
+++ jquery.input-hint.js
@@ -0,0 +1,74 @@
+
+/**
+ * inputHint
+ *
+ * jQuery plugin to display form input hints directly and dynamically as value
+ * of the input.
+ *
+ * Basic usage:  On the inputs add 'inputHint' to the class attribute, enter the
+ * hint text as the title attribute and call $.inputHint();
+ *
+ * Password inputs are handled intelligently. Works on text and password inputs.
+ * (Textarea probably coming soon...)
+ *
+ * (c) 2010, Marc Diethelm
+ */
+
+(function($) {
+
+	$.inputHint = function() {
+
+		$("input.inputHint").each(function() {
+
+			var $this = $(this),
+				hint = this.getAttribute("title");
+				is_password = (this.getAttribute("type") == "password" ? true : false);
+
+			if ( this.value == "" ) {
+				this.setAttribute("value", hint);
+			}
+
+			if ( is_password ) {
+				this.setAttribute("type", "text");
+			}
+
+			$this
+			.data("inputHint", {hint: hint, is_password: is_password})
+			.removeAttr("title");
+
+		})
+		.bind("focus", inputFocusHandler)
+		.bind("blur", inputFocusHandler);
+	};
+
+
+	$.fn.inputHint = function() {
+
+	};
+
+
+	function inputFocusHandler( event ) {
+
+		var input = event.target,
+			$input = $(input),
+			hintData = $input.data("inputHint");
+
+		if ( event.type == "blur" && input.value == "" ) {
+			input.value = hintData.hint;
+
+			if ( hintData.is_password ) {
+				input.setAttribute("type", "text");
+			}
+
+		} else if ( event.type == "focus" && input.value == hintData.hint ) {
+			input.value = "";
+
+			if ( hintData.is_password ) {
+				input.setAttribute("type", "password");
+			}
+		}
+
+		return true;
+	}
+
+})(jQuery);
